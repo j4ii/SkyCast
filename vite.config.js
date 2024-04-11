af682e2b87626272315a21cfdbfd88d7a85be3ab
Jai <jaix5490@gmail.com>
2024-04-12 00:20:19 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// export default defineConfig({
-//   plugins: [react()],
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { copy } from "vite-plugin-copy";
@@ -10,7 +5,7 @@ export default defineConfig({
   plugins: [
     react(),
     copy({
-      targets: [{ src: "public", dest: "dist/public" }],
+      targets: [{ src: "public", dest: "dist" }],
     }),
   ],
 });
