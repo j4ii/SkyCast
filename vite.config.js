5e6bb03098298882a6bb18d7784cfbcbaef116bb
Jai <jaix5490@gmail.com>
2024-04-11 20:54:38 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import copy from "vite-plugin-copy";
+import { VitePluginCopy } from "vite-plugin-copy";
 export default defineConfig({
   plugins: [
     react(),
-    copy({
+    VitePluginCopy({
       targets: [{ src: "src/assets/**/*", dest: "dist/assets" }],
     }),
   ],
