01aca9d773daf4b94e186065bf57e9d2e2a70811
Jai <jaix5490@gmail.com>
2024-04-11 23:37:29 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,5 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import { copy } from 'vite-plugin-copy';
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     copy({
-//       targets: [{ src: 'src/assets/**/*', dest: 'dist/assets' }]
-//     })
-//   ]
-// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { copy } from 'vite-plugin-copy';
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [{ src: 'src/assets', dest: 'dist/assets' }]
-    })
-  ]
+  plugins: [react()],
 });
