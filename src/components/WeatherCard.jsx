1d9f07b909036bac0659661f7b3f6afd0a0d8de2
Jai <jaix5490@gmail.com>
2024-04-13 09:38:13 +0530

    modified:   .gitignore 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/WeatherCard.jsx 	modified:   vite.config.js

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 function WeatherCard({ weatherData }) {
   const city = {
     fontSize: "3rem",
@@ -7,7 +6,7 @@ function WeatherCard({ weatherData }) {
   return (
     <div className="card">
       <div className="weatherImg">
-        <img src={`public/assets/${weatherData.weather[0].icon}.png`} />
+        <img src={`./public/assets/${weatherData.weather[0].icon}.png`} />
       </div>
       <div className="weatherData">
         <h1>{(weatherData.main.temp - 273.15).toFixed(1)}°C</h1>
@@ -15,14 +14,14 @@ function WeatherCard({ weatherData }) {
         <h2 className="sky">{weatherData.weather[0].description}</h2>
         <div>
           <span className="x">
-            <img src="public/assets/humidity.png" alt="humidity" />
+            <img src={"./public/assets/humidity.png"} alt="humidity" />
             <span>
               <h2>{weatherData.main.humidity}%</h2>
               <h2>Humidity</h2>
             </span>
           </span>
           <span className="x">
-            <img src="public/assets/wind.png" alt="wind" />
+            <img src={"./public/assets/wind.png"} alt="wind" />
             <span>
               <h2>{weatherData.wind.speed}m/s</h2>
               <h2>Wind Speed</h2>
